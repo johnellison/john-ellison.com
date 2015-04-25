72716723fff9cf17fc5e9902a38d68265fbd91fb
John Ellison <info@johnwebsites.com>
2015-04-25 16:15:38 -0600

    added vimeo ga tracking

--- app/scripts/ga-events.js
+++ app/scripts/ga-events.js
@@ -1,14 +1,15 @@
 (function() {
   'use strict';
 
-  //contact button-video
-  $("[data-title='play']").on('click', function(){
-    ga('send', {
-      'hitType' : 'event',
-      'eventCategory' : 'Video',
-      'eventAction' : 'Play'
-    });
-  });
+  //vimeo analytics events
+  ga('send', 'event', 'Video', 'Started video', 'Journey & Passion', undefined, {'nonInteraction': 1});
+  ga('send', 'event', 'Video', 'Paused video', 'Journey & Passion', undefined, {'nonInteraction': 1});
+  ga('send', 'event', 'Video', 'Resumed video', 'Journey & Passion', undefined, {'nonInteraction': 1});
+  ga('send', 'event', 'Video', 'Completed video', 'Journey & Passion', undefined, {'nonInteraction': 1});
+  ga('send', 'event', 'Video', 'Skipped video forward or backward', 'Journey & Passion', undefined, {'nonInteraction': 1});
+  ga('send', 'event', 'Video', 'Played video: 25%', 'Journey & Passion', undefined, {'nonInteraction': 1});
+  ga('send', 'event', 'Video', 'Played video: 50%', 'Journey & Passion', undefined, {'nonInteraction': 1});
+  ga('send', 'event', 'Video', 'Played video: 75%', 'Journey & Passion', undefined, {'nonInteraction': 1});
 
   //contact button
   $("[data-event='contact-button']").on('click', function(){
