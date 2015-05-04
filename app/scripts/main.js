dfd41f3686c8f019011338892aec9f1151f23881
John Ellison <info@johnwebsites.com>
2015-05-04 14:45:31 -0600

    add escape key into function called after video is opened

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -10,6 +10,7 @@
 
     $( '#video-modal' ).foundation( 'reveal', 'open' );
     $( 'iframe' ).attr( 'src', '//player.vimeo.com/video/121852108?autoplay=1&color=7e8b9e&title=0&byline=0&portrait=0' );
+    listenForEscape();
   });
 
   $( '.close-reveal-modal ').click( function ( e ) {
@@ -19,11 +20,14 @@
     $( 'iframe').attr('src', '//player.vimeo.com/video/121852108?autoplay=0&color=7e8b9e&title=0&byline=0&portrait=0');
   });
 
-  $( document ).keyup( function ( e ) {
-    if ( e.keyCode == 27 ) {
-      $( 'iframe').attr('src', '//player.vimeo.com/video/121852108?autoplay=0&color=7e8b9e&title=0&byline=0&portrait=0');
-    }
-  });
+  function listenForEscape() {
+    $( document ).keyup( function ( e ) {
+      if ( e.keyCode == 27 ) {
+        $( 'iframe').attr('src', '//player.vimeo.com/video/121852108?autoplay=0&color=7e8b9e&title=0&byline=0&portrait=0');
+      }
+    });
+  }
+  
 
 })( jQuery );
 
