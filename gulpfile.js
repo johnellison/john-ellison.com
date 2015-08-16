d8f3b6058926a14597841e65af2715458e56bbf5
John Ellison <info@johnwebsites.com>
2015-08-16 17:39:18 +0100

    update welcome, meta, and next chapter content

--- gulpfile.js
+++ gulpfile.js
@@ -120,7 +120,7 @@ gulp.task('wiredep', function () {
     .pipe(gulp.dest('app'));
 });
 
-gulp.task('build', ['jshint', 'html', 'images', 'fonts', 'extras', 'uncss'], function () {
+gulp.task('build', ['jshint', 'html', 'images', 'fonts', 'extras', 'uncss', 'pdf'], function () {
   return gulp.src('dist/**/*').pipe($.size({title: 'build', gzip: true}));
 });
 
