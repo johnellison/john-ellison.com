6c0ae9c822e1635d08ac50d8793a4208f2fbdcc9
John Ellison <info@johnwebsites.com>
2015-12-02 08:20:10 +0000

    re-install foundation under bower

--- app/scripts/ga-events.js
+++ app/scripts/ga-events.js
@@ -1,32 +1,21 @@
 (function() {
   'use strict';
 
-  //vimeo analytics events
-  ga('send', 'event', 'Video', 'Started video', 'Journey & Passion', undefined, {'nonInteraction': 1});
-  ga('send', 'event', 'Video', 'Paused video', 'Journey & Passion', undefined, {'nonInteraction': 1});
-  ga('send', 'event', 'Video', 'Resumed video', 'Journey & Passion', undefined, {'nonInteraction': 1});
-  ga('send', 'event', 'Video', 'Completed video', 'Journey & Passion', undefined, {'nonInteraction': 1});
-  ga('send', 'event', 'Video', 'Skipped video forward or backward', 'Journey & Passion', undefined, {'nonInteraction': 1});
-  ga('send', 'event', 'Video', 'Played video: 25%', 'Journey & Passion', undefined, {'nonInteraction': 1});
-  ga('send', 'event', 'Video', 'Played video: 50%', 'Journey & Passion', undefined, {'nonInteraction': 1});
-  ga('send', 'event', 'Video', 'Played video: 75%', 'Journey & Passion', undefined, {'nonInteraction': 1});
-
   //contact button
-  $("[data-event='contact-button']").on('click', function(){
+  $('[data-event="contact-button"]').on('click', function(){
     ga('send', {
-      'hitType' : 'event',
-      'eventCategory' : 'Contact',
-      'eventAction' : 'sent',
-      'eventLabel' : 'Contact Me',
-      'eventValue' : 400
+      'hitType': 'event',
+      'eventCategory': 'Contact',
+      'eventAction': 'sent',
+      'eventLabel': 'Contact Me',
+      'eventValue': 400
     });
   });
 
-  $("[data-event='view-resume'").on('click', function() {
+  $('[data-event="view-resume"').on('click', function() {
     ga('send', {
-      'hitType' : 'event',
-      'eventCategory' : 'View Resume'
+      'hitType': 'event',
+      'eventCategory': 'View Resume'
     });
   });
 }());
- 
--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,45 +1,44 @@
 /* jshint devel:true */
 
 (function( $ ){
-  'use strict'; 
-
-  $( document ).foundation();
-
-  // video modal on index
-  $( '.play' ).click( function ( e ) {
-    e.preventDefault();
-
-    $( '#video-modal' ).foundation( 'reveal', 'open' );
-    $( 'iframe' ).attr( 'src', '//player.vimeo.com/video/121852108?autoplay=1&color=7e8b9e&title=0&byline=0&portrait=0' );
-    listenForEscape();
-  });
-
-  $( '.close-reveal-modal ').click( function ( e ) {
-    e.preventDefault();
-
-    $( '#video-modal' ).foundation( 'reveal', 'close' );
-    $( 'iframe').attr('src', '//player.vimeo.com/video/121852108?autoplay=0&color=7e8b9e&title=0&byline=0&portrait=0');
-  });
+  'use strict';
 
   function listenForEscape() {
     $( document ).keyup( function ( e ) {
-      if ( e.keyCode == 27 ) {
+      if ( e.keyCode === 27 ) {
         $( 'iframe').attr('src', '//player.vimeo.com/video/121852108?autoplay=0&color=7e8b9e&title=0&byline=0&portrait=0');
       }
     });
   }
 
+  $( document ).foundation();
+
+  // video modal on index
+  $('.play').click( function ( e ) {
+    e.preventDefault();
+
+    $('#video-modal').foundation( 'reveal', 'open' );
+    $('iframe').attr( 'src', '//player.vimeo.com/video/121852108?autoplay=1&color=7e8b9e&title=0&byline=0&portrait=0' );
+    listenForEscape();
+  });
+
+  $('.close-reveal-modal').click( function ( e ) {
+    e.preventDefault();
+
+    $('#video-modal').foundation( 'reveal', 'close' );
+    $('iframe').attr('src', '//player.vimeo.com/video/121852108?autoplay=0&color=7e8b9e&title=0&byline=0&portrait=0');
+  });
 
   // Hides the extra portfolio items on load
   $('.m-portfolio-list').hide();
   $('#projects').show();
 
   // Slide toggles when clicked...
-  $( '#projects' ).click( function ( e ) {
+  $('#projects').click( function ( e ) {
     var $this = $(this);
     e.preventDefault();
     $('.m-portfolio-list').slideToggle( 'slow', function() {
-      if ($this.text() == 'Hide More Projects') {
+      if ($this.text() === 'Hide More Projects') {
         $this.text('View More Projects');
       } else {
         $this.text('Hide More Projects');
@@ -53,4 +52,3 @@
   })();
 
 })( jQuery );
-
